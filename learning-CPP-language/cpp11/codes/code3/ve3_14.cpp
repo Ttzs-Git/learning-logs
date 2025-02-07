c14a9fc34ee51c4a5fe663dcaa61b0fe705f4fe7
Ttzs-Git <b24041820@njupt.edu.cn>
2025-02-07 14:02:36 +0800

    notes add:add somes notes about vector;codes add:add some notes about vector

---
+++ learning-CPP-language/cpp11/codes/code3/ve3_14.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<string> vstring;
+    string str;
+    while(cin>>str)
+    {
+        vstring.push_back(str);
+    }
+}
---
+++ learning-CPP-language/cpp11/codes/code3/vector1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<string> vstring{"an","ann","annn"};
+    cout<<vstring[0]<<endl;
+    return 0;
+}
