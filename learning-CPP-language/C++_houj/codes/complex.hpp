fd840efcac293ae70769c602ed1ef2597c53225b
Ttzs-Git <b24041820@njupt.edu.cn>
2025-04-09 16:56:46 +0800

    notes add:cpp about object and recommendation

---
+++ learning-CPP-language/C++_houj/codes/complex.hpp
@@ -0,0 +1,14 @@
+#ifndef _COMPLEX_
+#define _CPMPLEX_
+class complex
+{
+    private:
+    double re,im;
+    public:
+    // 列表初始化构造函数
+    complex(double a=0,double b=0):re(a),im(b)
+    {}
+    
+
+};
+#endif
---
+++ learning-CPP-language/C++_houj/codes/string_test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a,b;
+    auto x=sizeof(a);
+    auto y=sizeof(b);
+    cout<<x<<y<<endl;
+    return 0;
+}
