9bbc78dbf143f4d5ea20db85d2a33e2040a05a47
Ttzs-Git <b24041820@njupt.edu.cn>
2025-04-17 21:58:14 +0800

    all add:essential c++ until sunday 10p.m.

---
+++ learning-CPP-language/cpp/codes/codes_essential/li1_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"请输入你的名字:"<<"\n";
+    cin>>name;
+    cout<<"Hello,"
+        <<name
+        <<"\n"
+        <<"goodbye"
+        <<"\n";
+    return 0;
+}
---
+++ learning-CPP-language/cpp/codes/codes_essential/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<complex>
+using namespace std;
+
+int main()
+{
+    complex<double> a(0,7);
+    cout<<a;
+    return 0;
+}
