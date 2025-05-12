bb89f4108027cf4dcbb1d9d257a939cedadecc25
Ttzs-Git <b24041820@njupt.edu.cn>
2025-05-12 23:17:36 +0800

    Revert "codes add:algo c++ HDU203"
    This reverts commit fb37739767b470e647ffd78543586b4f13147b05.

--- learning-CPP-language/cpp-algo/HDU_100/2032.cpp
+++ learning-CPP-language/cpp-algo/HDU_100/2032.cpp
@@ -1,37 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 // HDU 2032
 void sjx(long long n)
 {
     vector<vector<long long>> vll;
-    for (int i = 0; i != n; ++i)
+    for(int i=0;i!=n;++i)
     {
         vector<long long> vl;
         vl.clear();
-        for (int j = 0; j != i + 1; ++j)
+        for(int j=0;j!=i+1;++j)
         {
-            if (j == 0 || j == i)
-                vl.push_back(1);
-            else
-                vl.push_back(vll[i - 1][j - 1] + vll[i - 1][j]);
-            cout << vl[j];
-            if (j != i)
-                cout << " ";
-            else
-                cout << endl;
+            if(j==0||j==i) vl.push_back(1);
+            else vl.push_back(vll[i-1][j-1]+vll[i-1][j]);
+            cout<<vl[j];
+            if(j!=i) cout<<" ";
+            else cout<<endl;
         }
         vll.push_back(vl);
+    
+
     }
 }
+
 
 int main()
 {
     int n;
-    while (cin >> n)
+    while(cin>>n)
     {
         sjx(n);
-        cout << endl;
+        cout<<endl;
     }
     return 0;
 }
